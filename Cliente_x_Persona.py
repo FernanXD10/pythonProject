fac94552e6dcd3f189ff5e6d6d72017599b8e47e
FernanXD10 <159392753+FernanXD10@users.noreply.github.com>
2024-02-17 17:11:17 -0500

    Update Cliente_x_Persona.py

--- Cliente_x_Persona.py
+++ Cliente_x_Persona.py
@@ -1,8 +1,16 @@
-class Personas:
-    def __init__(self, nombre, edad, email=None):
+class Persona:
+    '''
+    Crear los objetos de tipo Persona
+    '''
+    def __init__(self, nombre, genero, ocupacion=None, cedula=None):
         self._nombre = nombre
-        self._edad = edad
-        self._email = email
+        self._genero = genero
+        self._ocupacion = ocupacion
+        self._cedula = cedula
+
+    def __str__(self):
+        return (f'Persona: [nombre: {self._nombre}, genero: {self._genero}, '
+                f'ocupación: {self._ocupacion}, cedula: {self._cedula}]')
 
     @property
     def nombre(self):
@@ -13,33 +21,51 @@ class Personas:
         self._nombre = nombre
 
     @property
-    def edad(self):
-        return self._edad
+    def genero(self):
+        return self._genero
 
-    @edad.setter
-    def edad(self, edad):
-        self._edad = edad
+    @genero.setter
+    def genero(self, genero):
+        self._genero = genero
 
     @property
-    def email(self):
-        return self._email
+    def ocupacion(self):
+        return self._ocupacion
 
-    @email.setter
-    def email(self, email):
+    @ocupacion.setter
+    def ocupacion(self, ocupacion):
+        self._ocupacion = ocupacion
+
+    @property
+    def cedula(self):
+        return self._cedula
+
+    @cedula.setter
+    def cedula(self, cedula):
+        self._cedula = cedula
+
+    def saludar(self):
+        print(f'Hola soy {self._nombre}')
+
+
+class Cliente(Persona):
+    '''
+    Crear los objetos de tipo Cliente
+    '''
+    def __init__(self, nombre, genero, ocupacion, cedula, email=None):
+        super().__init__(nombre, genero, ocupacion, cedula)
         self._email = email
 
     def __str__(self):
-        return f"Persona: {self.__dict__}"
-
-
-class Cliente(Personas):
-    def __str__(self):
-        return f"Cliente: {self.__dict__}"
+        return (f'Cliente: [nombre: {self._nombre}, genero: {self._genero}, '
+                f'ocupación: {self._ocupacion}, cedula: {self._cedula}, '
+                f'email: {self._email}]')
 
 
 if __name__ == '__main__':
-    persona1 = Personas("Juan", 30)
-    print(persona1)
-
-    cliente1 = Cliente("Maria", 25, "maria@example.com")
+    cliente1 = Cliente('Maria', 'F', 'Tecnolog', '1234567890', 'maria@example.com')
     print(cliente1)
+
+    cliente2 = Cliente('Juan', 'M', 'Estudiante', '0987654321')
+    cliente2.email = 'juan@example.com'
+    print(cliente2)
